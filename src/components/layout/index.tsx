eb4d9d3baa0d0d7e95a9ddc1fa227f6e1d20229e
Sarthak Verma <55709030+sarthakvdev@users.noreply.github.com>
2023-01-23 13:07:04 +0530

    re-route to home on disconnection

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,23 +1,33 @@
-import { ReactNode, FC } from 'react';
+import { ReactNode, FC, useEffect } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import NetworkStatus from './NetworkStatus';
+import { useAccount } from 'wagmi';
+import { useRouter } from 'next/router';
 
 interface Props {
-	children: ReactNode;
+  children: ReactNode;
 }
 
 const Layout: FC<Props> = (props) => {
-	return (
-		<div className="flex flex-col min-h-screen">
-			<Header />
-			<div className="text-center">{props.children}</div>
-			<div className="fixed bottom-1 right-1">
-				<NetworkStatus />
-			</div>
-			<Footer />
-		</div>
-	);
+  const { isDisconnected } = useAccount();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isDisconnected) {
+      router.push('/');
+    }
+  }, [isDisconnected]);
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <div className="text-center">{props.children}</div>
+      <div className="fixed bottom-1 right-1">
+        <NetworkStatus />
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Layout;
